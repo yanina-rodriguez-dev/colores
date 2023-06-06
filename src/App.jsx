a60b45838a58767a0963c432633e75e4de284b6a
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-05 22:17:16 -0300

    agrego estructura del formuilario e imports en app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from 'react-bootstrap'
+import Formulario from "./components/Formulario"
 
 function App() {
  
 
   return (
     <>
-     
+     <Container className="py-4">
+       <h1>Administrar colores</h1>
+       <hr />
+       <Formulario></Formulario>
+     </Container>
       
     </>
   )
